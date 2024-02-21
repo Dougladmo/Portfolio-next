192c2e11c87ca23a501b5f3ecf68fda61d72d6f5
dougladmo <Douglas.costa7417@gmail.com>
2024-02-21 20:10:00 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar";
+import Transition from '@/components/Transition'
 
 export default function Home() {
   return (
     <main className="text-white min-h-screen truncate">
       <h1>Home</h1>
+      <Transition />
     </main>
   );
 }
--- src/components/Transition.tsx
+++ src/components/Transition.tsx
@@ -1,4 +1,4 @@
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 
 // variants
 const transitionVariants = {
@@ -19,7 +19,15 @@ const transitionVariants = {
 const Transition = () => {
   return (
     <>
-        <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2e2257]' variants={transitionVariants} initial='initial' animate='animate' exit='exit' transition={{delay: 0.2, duration: 0.6, ease: 'easeInOut'}}>1</motion.div>
+        {/* <motion.div 
+        className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2e2257]' 
+        variants={transitionVariants} initial='initial' 
+        animate='animate'
+        exit='exit' 
+        transition={{delay: 0.2, duration: 0.6, ease: 'easeInOut'}}
+        >
+        1
+        </motion.div> */}
         <div>2</div>
         <div>3</div>
     </>
